ec4210bced50fa8f8524bd1098a72afbe2623884
Wael Hourani <wael.business@outlook.com>
2023-03-08 10:48:04 +1100

    commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express';
-
 import helmet from 'helmet';
-
+import cors from 'cors';
 import morgan from 'morgan';
 import axios from 'axios';
 import path from 'path';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.set('Cross-Origin-Resource-Policy', 'same-origin');
+  next();
+});
 
 app.use(
   helmet({
