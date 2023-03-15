e3c9e5a477506cc51d4bef470ad8825f57d08dd6
Wael Hourani <wael.business@outlook.com>
2023-03-15 20:26:05 +1100

    commit

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const API_KEY = 'AIzaSyA-KRW4MlLYE3hXWgauOew4pk1ddujgTIk'
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/api/place/:placeId/reviews', async (req, res) => {
   const placeId = req.params.placeId;
   const url= `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${API_KEY}`;
